21b9b1f1e81b9ea5251ad52d0cbcba1cee126f37
ElodieBa <baudu.elodie@gmail.com>
2019-05-04 17:22:12 +0200

    Premier commit essai pull

---
+++ src/main/java/com/fr/adaming/demoSpringBoot/EssaiPullApplication.java
@@ -0,0 +1,13 @@
+package com.fr.adaming.demoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssaiPullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssaiPullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fr/adaming/demoSpringBoot/EssaiPullApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fr.adaming.demoSpringBoot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EssaiPullApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
